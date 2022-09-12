6130ab25b0cd179f85120d99f473e9927b3f03f2
luis Ruben <56692766+luchoGit952@users.noreply.github.com>
2022-09-12 09:11:20 -0500

    prueba 102

--- servicio-completado.php
+++ servicio-completado.php
@@ -1,7 +1,7 @@
 <?php include'layout/nav-home-conductor.php';
 $id__conductor = isset($_GET['idc']) ? $_GET['idc'] : '';
 if($id__conductor){
-    $consulta__datos__servicios = "SELECT * FROM datos__inicio__recorrido WHERE id_conductor = '$id__conductor' LIMIT 1";
+    $consulta__datos__servicios = "SELECT * FROM datos__inicio__recorrido WHERE id_conductor = '$id__conductor'";
     $ejecutar__consulta = mysqli_query($conexion__db__accent,$consulta__datos__servicios);
 
 
@@ -12,68 +12,69 @@ if($id__conductor){
     <h2 class="subtitulo__solicitudes">Servicios completados</h2>
     <p class="texto__servicio__completado">Toda la información de tus servicios completados apareceran aqui .
     </p>
-    <br><br>
-    <?php                              
-    
-    if(mysqli_num_rows($ejecutar__consulta) > 0){
+    <div class="contenido__servicio__completado">
+<?php    
+
+      if(mysqli_num_rows( $ejecutar__consulta) > 0){
         while($fila = mysqli_fetch_array($ejecutar__consulta)){       
-            if($fila['estado_recorrido'] === "Recorrido terminado"){
-            $consulta__datos__usuario = "SELECT avatar FROM  usuarios WHERE id_usuario = '{$fila['id_usuario']}'";  
-             $ejecutar__consulta__datos__usuario = mysqli_query($conexion__db__accent,$consulta__datos__usuario);
-             $resultado = mysqli_fetch_array($ejecutar__consulta__datos__usuario);?>
-        
-        <div class="datos__de__servicio__completado">
+          $consulta__datos__usuario = "SELECT avatar FROM  usuarios WHERE id_usuario = '{$fila['id_usuario']}'";  
+          $ejecutar__consulta__datos__usuario = mysqli_query($conexion__db__accent,$consulta__datos__usuario);
+          $resultado = mysqli_fetch_array($ejecutar__consulta__datos__usuario);?>
+     <div class="datos__solicitud__completada" data-aos="zoom-in">
      <div class="card__solicitudes">
      <div class="img">
-       <img src="upload/<?php echo $resultado['avatar'] ?>" class="imagen__de_perfil">
+     <img src="upload/<?php echo $resultado['avatar'] ?>" class="imagen__de_perfil">
     </div>
     <div class="infos">
       <div class="name">
-   <h2>Datos del usuario</h2> <?php                 
+      <h2>Datos del usuario</h2> 
+      <?php                 
       $consulta__datos__usuario = "SELECT nombre_usuario, primer_apellido,segundo_apellido,email,numero_documento,numero_telefono 
      FROM  usuarios WHERE id_usuario = '{$fila['id_usuario']}'";
       $ejecutar__consulta__datos__usuario = mysqli_query($conexion__db__accent,$consulta__datos__usuario);
-      $resultado = mysqli_fetch_array($ejecutar__consulta__datos__usuario);?> 
-   <h4><?php echo $resultado['primer_apellido'] ?> <?php echo $resultado['segundo_apellido'] ?></h4>
+      $resultado = mysqli_fetch_array($ejecutar__consulta__datos__usuario);?>
+   <h4><?php echo $resultado['nombre_usuario'] ?></h4>
+    <h4><?php echo $resultado['primer_apellido'] ?> <?php echo $resultado['segundo_apellido'] ?></h4>
    <h4><?php echo $resultado['email'] ?></h4>
    <h4><?php echo $resultado['numero_documento'] ?></h4>
    <h4><?php echo $resultado['numero_telefono'] ?></h4>
-   <h2>Datos del servicio a tomar</h2>
+   <h2>Datos del servicio completado</h2>
    <h4>  <?php echo $fila['direccion_inicio']  ?></h4>
    <?php echo $fila['fecha_inicio'] ?>
    <?php echo date('g:i:a',strtotime($fila['hora_inicio'])) ?>
-   <br><br>
-   <h2>Información del servicio completado</h2>
-   <?php   $consulta__detalles__compra = "SELECT * FROM detalles__de__la__compra WHERE id_conductor = '$id__conductor' ";  
+   <?php $consulta__detalles__compra = "SELECT * FROM detalles__de__la__compra WHERE id_conductor = '$id__conductor' ";  
          $ejecutar__consulta__compra = mysqli_query($conexion__db__accent,$consulta__detalles__compra);
         if(mysqli_num_rows($ejecutar__consulta) > 0)
        $fila__resultado = mysqli_fetch_array($ejecutar__consulta__compra)?>
+
+   <br><br>
+   <h2>Datos adicionales</h2>
   </div>
 
   <p class="text">
-  <h4> <?php echo $fila__resultado['descripcion'] ?></h4>
+    <h4><?php echo $fila__resultado['descripcion'] ?><h4> 
     </p>
 
   <ul class="stats">
   <?php  if($fila['estado_recorrido'] === "Recorrido terminado"){?>
-        <p class="parrafo__estado culminado" id="estado">Estado solicitud : <?php echo $fila['estado_recorrido']  ?></p>
+        <p class="parrafo__estado culminado estado__terminado" id="estado">Estado solicitud : <?php echo $fila['estado_recorrido']  ?></p>
         <?php } ?>
   </ul>
   <div class="links">
      <!-- <button class="follow">Follow</button>
      <button class="view">View profile</button> -->
-   
 
   </div>
 </div>
-
       </div>
+      </div>
+     
       <?php } ?>
-    <?php } ?>
-    <?php } ?>
-  
- 
-    
+      <?php } ?>
+      
+      
+      
+    </div>
 </div>
 
 
